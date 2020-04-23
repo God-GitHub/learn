2726abf5d94435ac8077fcfe8936904ed99f0cfc
chenlin <962230880@qq.com>
2020-04-23 17:13:06 +0800

    锁

--- MySQL/事务隔离.sql
+++ MySQL/事务隔离.sql
@@ -2,7 +2,7 @@
 SELECT @@transaction_isolation;
 # 设置当前事务隔离级别read uncommitted、read committed、repeatable read、serializable
 set session transaction isolation level read uncommitted;
-# 设置事务隔离级别read-uncommitted、read-committed、repeatable-read
+# 设置事务隔离级别read-uncommitted、read-committed、repeatable-read serializable
 set global transaction_isolation = 'repeatable-read';
 # 开始事务处理
 start transaction;
